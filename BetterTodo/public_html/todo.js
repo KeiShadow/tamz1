4d3857dba28c66c7eebe8152f5667985b0ab50eb
Petr Noga <n.skypo@gmail.com>
2017-03-07 20:33:32 +0100

    Funkční 2
    Předěláno do objektů, zatím nefunguje mazání a má hodně bugů, když nemá
    žádná data v JSONU tak se bugne stránka, je to na chuja.

--- BetterTodo/public_html/todo.js
+++ BetterTodo/public_html/todo.js
@@ -33,30 +33,40 @@ var indx = localStorage.length;
  
 function saveTodo()
 {
-    if($("#taskName").val()!=""){
+    var myTask, myObj,i,count;
+ if($("#taskName").val()!==""){
         var taskName = $("#taskName").val();
         var taskNote = $("#taskNote").val();
         var date = $("#date").val();
         var volume =$("#slider-1").val();
         var vibration= $("#flip-1").val();
-        var pr=getPriority();
-        var  text ={"Name":""+taskName+"","Note":""+taskNote+"","Date":""+date+"","Priority":""+pr+"","Volume":""+volume+"","Vibration":""+vibration+""};
-
-       var myTask = JSON.stringify(text);
-        localStorage.setItem("myNote",myTask);   
+        var pr= getPriority();
+        myObj ={
+          "task":{ "Name":""+taskName+"","Note":""+taskNote+"","Date":""+date+"","Priority":""+pr+"","Volume":""+volume+"","Vibration":""+vibration+""}
+        };
+        count=0;
+        i=0;
+        myTask = JSON.stringify(myObj.task);
+        // localStorage.setItem("myNote"+i+"",myTask);  
+        localStorage.setItem("myNote",myTask);  
         showTodo(); 
-    }
+   }
    
     
 }
 
 function clearTodo()
 {
-  text = localStorage.getItem('myNote');
-  obj = JSON.parse(text);
-        
-    $("#set").empty();
-    $("#set").listview ("refresh");
+    var text, key, value, obj;
+  text = localStorage.getItem("myNote");
+ 
+  obj= JSON.parse(text,(key,value)=>{
+         return value;
+    });    
+    alert(obj.Name);
+    
+  $("#set").empty();
+  $("#set").listview ("refresh");
 }
 
 function removeTodo(id){
@@ -80,18 +90,23 @@ function removeTodo(id){
 
 function showTodo()
 {
-    text = localStorage.getItem('myNote');
-    obj = JSON.parse(text);
-  
+       var text,string,key,value, Name, Note, Date, Priority, Volume, Vibration;
+    text = localStorage.getItem("myNote");
+    var obj = new Object();
+   if(text!==""){
+    obj= JSON.parse(text,(key,value)=>{
+         return value;
+    });    
     var nextId = 1;
-  var content = '<div data-role="collapsible" id="set"'+nextId+'"><h3>'+obj.Name+'</h3><p>Note: <br>'+obj.Note+'<br> Date: <br> '+obj.Date+'<br> Priority: <br>'+obj.Priority+'<br> Volume: <br> '+obj.Volume+'<br> Vibration: <br>'+obj.Vibration+'</p></div>';
+    var content = '<div data-role="collapsible" id="set"'+nextId+'"><h3>'+obj.Name+'</h3><p>Note: <br>'+obj.Note+'<br> Date: <br> '+obj.Date+'<br> Priority: <br>'+obj.Priority+'<br> Volume: <br> '+obj.Volume+'<br> Vibration: <br>'+obj.Vibration+'</p></div>';
    $( "#set" ).append( content ).collapsibleset( "refresh" );
      nextId++;
-      
-      
-    
-    $("#set").listview();
+      $("#set").listview();
     $("#set").listview("refresh"); 
+   }
+   
+    
+   
       $( "#expand" ).click(function() {
         $("#set").children(":last").collapsible( "expand" );
     });
