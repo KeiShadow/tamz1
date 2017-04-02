41d67e1b2cf817dcdbdfab6690cb5f4602ae5436
Petr Noga <n.skypo@gmail.com>
2017-04-02 21:21:10 +0200

    Easy
    Function which getting JSON data from file, one input giving num in CZE
    the second converting from CZE to Currenc from selected list.

--- currency_converter/public_html/converter.js
+++ currency_converter/public_html/converter.js
@@ -1,31 +1,24 @@
-
-
-
-
-
-
-
-
-
-
-function select() {
-    var x = document.getElementById("Myselect").value;
-    
-}
-
-function GetMena(){
-  var mena ={"meny": 
-            {"country_label":"Austr\u00e1lie","curr_label":"dolar","unit":"1","code":"AUD","rate":"18.922"}
-};
-var x = new Object();
-for(var i =0;i<mena.length();i++){
-    
-}
-}
-
-
-
-
-
-
+$.getJSON('data.JSON',function(data){  
+    var pole= new Array();
+   for(var i=0; i< data['mena'].length;i++){
+       $('#Myselect').append('<option id='+i+' >'+data['mena'][i]['code']+'= '+data['mena'][i]['country_label']+'</option>');
+       pole[i] = data['mena'][i]['code']+' '+data['mena'][i]['rate'];
+   }
+   
+   $('#Myselect').change(function(){
+       var id
+        $('select option:selected').each(function(){
+           id = $(this).attr('id'); 
+        });
+        var rate= data['mena'][id]['rate'];
+        var x =$('#Current').val() * rate;
+        document.getElementById('Next').value = x + ' '+data['mena'][id]['code'];
+        console.log(x);
+       console.log(rate);
+   });
+   
+   
+   
+   
+}); 
 
