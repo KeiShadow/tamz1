be95cec2998ab0ca55d1bb3d6abe8470bcc979f0
Petr Noga <n.skypo@gmail.com>
2017-04-02 21:53:09 +0200

    Without file
    Getting json data from URL

--- currency_converter/public_html/converter.js
+++ currency_converter/public_html/converter.js
@@ -1,24 +1,20 @@
-$.getJSON('data.JSON',function(data){  
-    var pole= new Array();
-   for(var i=0; i< data['mena'].length;i++){
-       $('#Myselect').append('<option id='+i+' >'+data['mena'][i]['code']+'= '+data['mena'][i]['country_label']+'</option>');
-       pole[i] = data['mena'][i]['code']+' '+data['mena'][i]['rate'];
+//JSON data ze souboru do selectu + výpočet měny
+$.getJSON('http://homel.vsb.cz/~mor03/TAMZ/cnb_json.php',function(data){  
+   for(var i=0; i< data['data'].length;i++){
+       $('#Myselect').append('<option id='+i+' >'+data['data'][i]['code']+'= '+data['data'][i]['country_label']+'</option>');
    }
-   
    $('#Myselect').change(function(){
        var id
         $('select option:selected').each(function(){
            id = $(this).attr('id'); 
         });
-        var rate= data['mena'][id]['rate'];
+        var rate= data['data'][id]['rate'];
         var x =$('#Current').val() * rate;
-        document.getElementById('Next').value = x + ' '+data['mena'][id]['code'];
-        console.log(x);
-       console.log(rate);
+        document.getElementById('Next').value = x + ' '+data['data'][id]['code'];
+        
+        if($('#datum').val()===""){
+            $('#datum').append(data['data'][id]['code']+' '+data['date'] +', ');
+        }
+        
    });
-   
-   
-   
-   
 }); 
-
